ac1999f0a5dbeb40bd29d7cec3d4d2b4ed45d885
Ewnet-Reta <ewnetreta030@gmail.com>
2025-03-19 15:21:54 +0300

    Add files via upload

---
+++ CGPACalculator.java
@@ -0,0 +1,20 @@
+
+package cgpacalculator;
+
+class ewnet{
+    void ermi(){
+        int y=6;
+       int x=10;
+        y+=x;
+        System.out.print(y);
+    }
+}
+public class CGPACalculator {
+
+   
+    public static void main(String[] args) {
+        ewnet re = new ewnet();
+        re.ermi();
+    }
+    
+}
